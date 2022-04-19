684143705a39de88ab6127add5aefa5057edf89d
NZXL <63413558+nzxl101@users.noreply.github.com>
2022-04-19 13:46:00 +0200

    Multiple channel support

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-require('dotenv').config();
+const config = require("./config.json");
 const tmi = require("tmi.js");
 const Banchojs = require("bancho.js");
 const moment = require("moment");
@@ -13,12 +13,12 @@ const { MapStars, OsuPerformanceCalculator } = require("@rian8337/osu-difficulty
 let osuLink = /^(https:\/\/osu\.ppy\.sh\/beatmapsets\/)|([0-9]+)|\#osu^\/|([0-9]+)/g, osuMods = /^\+|(EZ)|(NF)|(HT)|(SD)|(HD)|(HR)|(DT)|(FL)|(RX)|(SO)/i, accessToken;
 
 (() => {
-    let twitch = new tmi.Client({ identity: { username: process.env["TWITCH_USERNAME"], password: process.env["TWITCH_PASSWORD"] }, channels: [process.env["TWITCH_CHANNEL"]] }),
-        bancho = new Banchojs.BanchoClient({ username: process.env["OSU_MASTER"], password: process.env["OSU_PASSWORD"], apiKey: process.env["OSU_API_KEY"] }),
+    let twitch = new tmi.Client({ identity: { username: config.credentials.twitch.username, password: config.credentials.twitch.password }, channels: Object.keys(config.users) }),
+        bancho = new Banchojs.BanchoClient({ username: config.credentials.osu.username, password: config.credentials.osu.password, apiKey: config.credentials.osu.apiKey }),
         discord = new Client({ intents: [ Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_PRESENCES, Intents.FLAGS.GUILD_MEMBERS, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.GUILD_WEBHOOKS, Intents.FLAGS.DIRECT_MESSAGES ] });
 
     if(!fs.existsSync("./access")) {
-        console.log(`https://osu.ppy.sh/oauth/authorize?client_id=${process.env["OSU_CLIENT_ID"]}&redirect_uri=http://localhost:3000&response_type=code&scope=public`);
+        console.log(`https://osu.ppy.sh/oauth/authorize?client_id=${config.credentials.osu.devApp.clientID}&redirect_uri=http://localhost:3000&response_type=code&scope=public`);
         http.createServer((req, res) => {
             let queryObject = url.parse(req.url, true).query;
             if(queryObject.code) {
@@ -29,8 +29,8 @@ let osuLink = /^(https:\/\/osu\.ppy\.sh\/beatmapsets\/)|([0-9]+)|\#osu^\/|([0-9]
                     method: "POST", 
                     url: `https://osu.ppy.sh/oauth/token`,
                     data: {
-                        "client_id": process.env["OSU_CLIENT_ID"],
-                        "client_secret": process.env["OSU_CLIENT_SECRET"],
+                        "client_id": config.credentials.osu.devApp.clientID,
+                        "client_secret": config.credentials.osu.devApp.clientSecret,
                         "code": queryObject.code,
                         "grant_type": "authorization_code",
                         "redirect_uri": "http://localhost:3000"
@@ -53,8 +53,8 @@ let osuLink = /^(https:\/\/osu\.ppy\.sh\/beatmapsets\/)|([0-9]+)|\#osu^\/|([0-9]
             method: "POST", 
             url: `https://osu.ppy.sh/oauth/token`,
             data: {
-                "client_id": process.env["OSU_CLIENT_ID"],
-                "client_secret": process.env["OSU_CLIENT_SECRET"],
+                "client_id": config.credentials.osu.devApp.clientID,
+                "client_secret": config.credentials.osu.devApp.clientSecret,
                 "refresh_token": fs.readFileSync("./access", "utf8"),
                 "grant_type": "refresh_token",
             },
@@ -72,16 +72,25 @@ let osuLink = /^(https:\/\/osu\.ppy\.sh\/beatmapsets\/)|([0-9]+)|\#osu^\/|([0-9]
     twitch.connect().then(async () => {
         console.log("Twitch connected");
         await bancho.connect().then(() => console.log("Bancho connected"));
-        await discord.login(process.env["DISCORD_TOKEN"]);
+        await discord.login(config.credentials.discord.token);
 
         discord.on("ready", () => console.log("Discord connected"));
 
         twitch.on("message", (channel, tags, message, self) => {
             message = message.split(" ");
+            let name;
+
+            Object.keys(config.users).forEach(v => {
+                if(v == channel.replace(/\#/, "")) {
+                    name = config.users[v]
+                }
+            });
+
+            if(!name) return;
 
             if(message[0] == "!np" || message[0] == "!nppp" || message[0] == "!pp") {
                 let currentlyPlaying;
-                discord.guilds.cache.get(process.env["DISCORD_GUILD"]).members.fetch(process.env["DISCORD_USER"]).then(user => {
+                discord.guilds.cache.get(config.credentials.discord.guild).members.fetch(name.discord).then(user => {
                     if(!user.presence) return;
                     user.presence.activities.forEach(x => {
                         if(x.name == "osu!" && x.type == "PLAYING") {
@@ -144,7 +153,7 @@ let osuLink = /^(https:\/\/osu\.ppy\.sh\/beatmapsets\/)|([0-9]+)|\#osu^\/|([0-9]
                     beatmapInfo = await MapInfo.getInformation({ beatmapID: beatmapCalc.id });
                     rating = await new MapStars().calculate({ map: beatmapInfo.map, mods: mods });
 
-                    bancho.getUser(process.env["OSU_USERNAME"]).sendMessage(`[${tags["mod"] == true ? "MOD" : tags["subscriber"] == true ? "SUB" : tags["badges"] && tags.badges["vip"] ? "VIP" : "VIEWER"}] ${tags["username"]} » [${message[0]} ${beatmapCalc.artist} - ${beatmapCalc.title} [${beatmapCalc.version}]]${message[1] && message[1].match(osuMods) ? " +"+message[1].replace(/^\+/, "").toUpperCase() : ""} | 95%: ${Math.round(await new OsuPerformanceCalculator().calculate({ stars: rating.pcStars, accPercent: 95 }).total)}pp | 98%: ${Math.round(await new OsuPerformanceCalculator().calculate({ stars: rating.pcStars, accPercent: 98 }).total)}pp | 99%: ${Math.round(await new OsuPerformanceCalculator().calculate({ stars: rating.pcStars, accPercent: 99 }).total)}pp | 100%: ${Math.round(await new OsuPerformanceCalculator().calculate({ stars: rating.pcStars, accPercent: 100 }).total)}pp | ${moment.utc(beatmapCalc.totalLength*1000).format("mm:ss")} - ★ ${Math.round(beatmapCalc.difficultyRating)} - ♫ ${(beatmapCalc.countNormal+beatmapCalc.countSlider+beatmapCalc.countSpinner)} - AR${beatmapCalc.diffApproach} - OD${beatmapCalc.diffOverall}`);
+                    bancho.getUser(name.osu).sendMessage(`[${tags["mod"] == true ? "MOD" : tags["subscriber"] == true ? "SUB" : tags["badges"] && tags.badges["vip"] ? "VIP" : "VIEWER"}] ${tags["username"]} » [${message[0]} ${beatmapCalc.artist} - ${beatmapCalc.title} [${beatmapCalc.version}]]${message[1] && message[1].match(osuMods) ? " +"+message[1].replace(/^\+/, "").toUpperCase() : ""} | 95%: ${Math.round(await new OsuPerformanceCalculator().calculate({ stars: rating.pcStars, accPercent: 95 }).total)}pp | 98%: ${Math.round(await new OsuPerformanceCalculator().calculate({ stars: rating.pcStars, accPercent: 98 }).total)}pp | 99%: ${Math.round(await new OsuPerformanceCalculator().calculate({ stars: rating.pcStars, accPercent: 99 }).total)}pp | 100%: ${Math.round(await new OsuPerformanceCalculator().calculate({ stars: rating.pcStars, accPercent: 100 }).total)}pp | ${moment.utc(beatmapCalc.totalLength*1000).format("mm:ss")} - ★ ${Math.round(beatmapCalc.difficultyRating)} - ♫ ${(beatmapCalc.countNormal+beatmapCalc.countSlider+beatmapCalc.countSpinner)} - AR${beatmapCalc.diffApproach} - OD${beatmapCalc.diffOverall}`);
                     twitch.say(channel, "/me Request sent!");
                 });
             }
