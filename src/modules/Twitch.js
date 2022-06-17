dcc72c01a59877d211540fbc1a74aac6bef67fa2
nzxl101 <yyy.noel.yyy@gmail.com>
2022-06-17 15:27:04 +0200

    oops

--- src/modules/Twitch.js
+++ src/modules/Twitch.js
@@ -25,9 +25,10 @@ module.exports = class Twitch {
                     if(object) {
                         let modsMatch = message.join("").replace(/^\+/, "").match(deps.Regex.beatmapMods), 
                         mods = message.length >= 1 && modsMatch ? modsMatch.join("").toUpperCase() : object.Player.mods.text,
+                        parsedMods = await deps.Bancho.parseMods(mods),
                         pp = await deps.Bancho.calculate(object.Beatmap.id, [{ mods: parsedMods, acc: 95 }, { mods: parsedMods, acc: 98 }, { mods: parsedMods, acc: 99 }, { mods: parsedMods, acc: 100 }]);
                         
-                        deps.twitchClient.say(channel, `/me [★] Playing » ${object.Beatmap.name} https://osu.ppy.sh/b/${object.Beatmap.id} ${mods ? "+"+mods : "+NM"} | 95%: ${pp[0].pp}pp | 98%: ${pp[1].pp}pp | 99%: ${pp[2].pp}pp | 100%: ${pp[3].pp}pp | ${deps.moment( mods.indexOf("DT") >= 1 || mods.indexOf("NC") >= 1 ? ((Math.round(pp[0].totalLength*0.67) * 100) / 100)*1000  : pp[0].totalLength*1000).format("mm:ss")} - ★ ${pp[0].stars} - ♫ ${pp[0].countNormal+pp[0].countSpinner+pp[0].countSlider} - AR ${pp[0].ar} - OD ${pp[0].od}`);
+                        deps.twitchClient.say(channel, `/me [★] Playing » ${object.Beatmap.name} https://osu.ppy.sh/b/${object.Beatmap.id} ${mods ? "+"+mods : "+NM"} | 95%: ${pp[0].pp}pp | 98%: ${pp[1].pp}pp | 99%: ${pp[2].pp}pp | 100%: ${pp[3].pp}pp | ${deps.moment( mods && mods.indexOf("DT") >= 1 || mods && mods.indexOf("NC") >= 1 ? ((Math.round(pp[0].totalLength*0.67) * 100) / 100)*1000  : pp[0].totalLength*1000).format("mm:ss")} - ★ ${pp[0].stars} - ♫ ${pp[0].countNormal+pp[0].countSpinner+pp[0].countSlider} - AR ${pp[0].ar} - OD ${pp[0].od}`);
                     } else {
                         deps.twitchClient.say(channel, "/me No data available at the moment.");
                     }
@@ -46,9 +47,9 @@ module.exports = class Twitch {
                 deps.database.all(`SELECT username FROM users WHERE twitch = \"${channel.replace(/#/, "")}\"`, async (err, rows) => {
                     if(err || rows.length <= 0) return;
 
-                    let parsedMods = await deps.Bancho.parseMods(mods.toString());
+                    let parsedMods = mods ? await deps.Bancho.parseMods(mods.join("")) : 0;
                     let pp = await deps.Bancho.calculate(map[0].beatmapId, [{ mods: parsedMods, acc: 95 }, { mods: parsedMods, acc: 98 }, { mods: parsedMods, acc: 99 }, { mods: parsedMods, acc: 100 }]);
-                    deps.banchoClient.getUser(rows[0].username).sendMessage(`[★] ${tags["username"]} » [https://osu.ppy.sh/b/${map[0].beatmapId} ${map[0].artist} ${map[0].title} [${map[0].version}]] ${mods ? "+"+mods.join("") : "+NM"} | 95%: ${pp[0].pp}pp | 98%: ${pp[1].pp}pp | 99%: ${pp[2].pp}pp | 100%: ${pp[3].pp}pp | ${deps.moment( mods.includes("DT") || mods.includes("NC") ? ((Math.round(map[0].totalLength*0.67) * 100) / 100)*1000  : map[0].totalLength*1000).format("mm:ss")} - ★ ${pp[0].stars} - ♫ ${map[0].countNormal+map[0].countSpinner+map[0].countSlider} - AR ${pp[0].ar} - OD ${pp[0].od}`);
+                    deps.banchoClient.getUser(rows[0].username).sendMessage(`[★] ${tags["username"]} » [https://osu.ppy.sh/b/${map[0].beatmapId} ${map[0].artist} ${map[0].title} [${map[0].version}]] ${mods ? "+"+mods.join("").toUpperCase() : "+NM"} | 95%: ${pp[0].pp}pp | 98%: ${pp[1].pp}pp | 99%: ${pp[2].pp}pp | 100%: ${pp[3].pp}pp | ${deps.moment( mods && mods.indexOf("DT") >= 1 || mods && mods.indexOf("NC") ? ((Math.round(map[0].totalLength*0.67) * 100) / 100)*1000  : map[0].totalLength*1000).format("mm:ss")} - ★ ${pp[0].stars} - ♫ ${map[0].countNormal+map[0].countSpinner+map[0].countSlider} - AR ${pp[0].ar} - OD ${pp[0].od}`);
                 });
             }
         });
