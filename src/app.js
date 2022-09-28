f409ecff9e41b222a724c68f95c0858243289417
nzxl101 <yyy.noel.yyy@gmail.com>
2022-09-28 16:19:52 +0200

    First step done

--- src/app.js
+++ src/app.js
@@ -1,41 +1,249 @@
-require('dotenv').config();
-const deps = require("./constants.js");
-require("log-prefix")(() => { return `[${deps.moment(Date.now()).format("HH:mm:ss")} | kiyomii's service]`; });
-process.on("unhandledRejection", error => console.error("Promise rejection:", error));
+// Utils
+const Regex = {
+    setId: /(?<=#osu\/|\/s\/)\d+/g,
+    beatmapId: /(?<=beatmapsets\/|b\/)\d+/g,
+    beatmapMods: /\+(NF|EZ|HD|HR|(SD|PF)|(NC|DT)|RX|HT|FL|SO)/ig
+};
+const moment = require("moment");
+const path = require("path");
+const fetch = require("node-fetch");
 
-setTimeout(() => {
-    process.exit();
-}, 12*60*60*1000);
+require("dotenv").config();
+require("log-prefix")(() => { return `[nzxl.space | ${moment(Date.now()).format("HH:mm:ss")}]`; });
+process.on("unhandledRejection", error => console.error(error));
+
+// MongoDB
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const mongoClient = new MongoClient(process.env.MONGODB, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+// tmi.js
+const tmi = require("tmi.js");
+const twitchClient = new tmi.Client({
+    identity: {
+        username: process.env.TWITCH_USERNAME,
+        password: process.env.TWITCH_PASSWORD
+    }
+});
+let twitch_accessToken;
+
+// osu! Bancho
+const Banchojs = require("bancho.js");
+const banchoClient = new Banchojs.BanchoClient({
+    username: process.env.OSU_USERNAME,
+    password: process.env.OSU_PASSWORD,
+    apiKey: process.env.OSU_API_KEY
+});
+const pp = require("rosu-pp");
+let osu_accessToken;
+
+// Discord
+const { Client, Intents } = require("discord.js");
+const discordClient = new Client({
+    partials: ["CHANNEL"],
+    intents: [ Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.GUILD_PRESENCES, Intents.FLAGS.DIRECT_MESSAGES, Intents.FLAGS.DIRECT_MESSAGE_TYPING ]
+});
+const DiscordOauth2 = require("discord-oauth2");
+const oauth = new DiscordOauth2();
+
+// Webserver
+const express = require("express");
+const { createServer } = require("http");
+const app = express();
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "static"));
+app.use(express.static(path.join(__dirname, "static")));
+const httpServer = createServer(app);
 
 (() => {
-    // Create db file if not exists
-    if(!deps.fs.existsSync("./storage.db")) {
-        deps.fs.writeFileSync("./storage.db", null);
-    }
+    let db, discordUsers;
+    mongoClient.connect(async err => {
+        if(err) return console.log("MongoDB failed!");
 
-    // Create maps folder if not exists
-    if(!deps.fs.existsSync("./maps/")) {
-        deps.fs.mkdirSync("./maps/");
-    }
+        db = mongoClient.db("osu");
+        discordUsers = await db.collection("users").distinct("userId");
 
-    // Load modules
-    deps.fs.readdir(deps.path.join(__dirname, "modules"), (err, files) => {
-        if(err) return console.log("No modules found");
+        console.log("MongoDB connected!");
+    });
 
-        const module = {};
+    banchoClient.connect();
+    banchoClient.on("connected", () => {
+        console.log(`Bancho connected as ${process.env.OSU_USERNAME}!`);
+        if(banchoClient.listeners("PM").length <= 0) {
+            banchoClient.on("PM", (data) => {
+                // listen to /np and calculate
+                // console.log(data.message);
+            });
+        }
+    });
 
-        files.forEach((file, i) => {
-            moduleName = files[i].replace(/.js/, "");
-            files[i] = require(deps.path.join(__dirname, "modules", file));
-            module[`${moduleName}`] = new files[i];
+    discordClient.login(process.env.DISCORD_TOKEN);
+    discordClient.on("ready", () => {
+        console.log(`Discord connected as ${discordClient.user.tag}!`);
+        discordClient.user.setPresence({ activities: [{ name: "osu!", type: "PLAYING" }], status: "dnd" });
+
+        // Get osu map from activity .. 
+        setInterval(() => {
+            for(let i = 0; i < discordUsers.length; i++) {
+                discordClient.guilds.cache.get(process.env.DISCORD_GUILD).members.cache
+                .filter(x => x.id == discordUsers[i])
+                .map(discordUser => {
+                    if(!discordUser.presence || discordUser.presence && discordUser.presence.activities.length <= 0) return;
+                    db.collection("users").find({ userId: discordUsers[i] }).toArray(async (err, result) => {
+                        if(err || result && result.length <= 0) return;
+                        let user = result[0],
+                            activity = discordUser.presence.activities.filter(x => x.name == "osu!");
+
+                        if(activity.length >= 1) {
+                            if(user.osu == null) {
+                                let osuUsername = activity[0].assets.largeText.match(/^\w+/);
+                                db.collection("users").updateOne({ userId: discordUsers[i] }, { $set: { osu: osuUsername[0] } }, (err, result) => {
+                                    if(err || !result) return;
+                                });
+                            }
+
+                            if(!twitchClient.getChannels().includes(`#${user.twitch}`) && await liveStatus(user.twitch) == true) {
+                                twitchClient.join(`#${user.twitch}`);
+                                console.log(`Listening for requests on #${user.twitch}`);
+                            }
+
+                            if(activity[0].details)
+                                console.log(await lookupBeatmap(activity[0].details));
+
+                        } else {
+                            if(twitchClient.getChannels().includes(`#${user.twitch}`)) {
+                                twitchClient.part(`#${user.twitch}`);
+                                console.log(`Left channel #${user.twitch}`);
+                            }
+                        }
+                    });
+                });
+            }
+        }, 15*1000);
+    });
+
+    twitchClient.connect();
+    twitchClient.on("connected", () => {
+        console.log(`Twitch connected as ${process.env.TWITCH_USERNAME}!`);
+        if(twitchClient.listeners("message").length <= 0) {
+            twitchClient.on("message", (channel, tags, message, self) => {
+                // listen to beatmap requests and !np command
+            });
+        }
+    });
+
+    httpServer.listen(2048, () => {
+        console.log(`Listening on port ${httpServer.address().port}!`);
+        app.get("/", (req, res) => {
+            res.render("index");
         });
 
-        
-        module["WebSocket"].createServer();
-        module["Bancho"].createBancho();
-        module["Discord"].createDiscord();
-        module["Twitch"].createTwitch();
+        app.get("/discord", (req, res) => {
+            if(!req.query.code) return;
 
-        deps.Bancho = module["Bancho"];
+            oauth.tokenRequest({
+                clientId: process.env.DISCORD_PUBLIC,
+                clientSecret: process.env.DISCORD_SECRET,
+                code: req.query.code,
+                scope: "identify guilds",
+                grantType: "authorization_code",
+                redirectUri: process.env.DISCORD_REDIRECT_URI
+            }).then((data) => {
+                if(!data || data && !data["access_token"]) return;
+
+                oauth.getUser(data.access_token).then(async (user) => {
+                    await oauth.addMember({
+                        accessToken: data.access_token,
+                        botToken: process.env.DISCORD_TOKEN,
+                        guildId: process.env.DISCORD_GUILD,
+                        userId: user.id
+                    });
+
+                    oauth.getUserConnections(data.access_token).then((c) => {
+                        db.collection("users").find({ userId: user.id }).toArray((err, result) => {
+                            if(err || result.length <= 0) {
+                                db.collection("users").insertOne({
+                                    userId: user.id,
+                                    discordName: `${user.username}#${user.discriminator}`,
+                                    twitch: `${c.filter(x => x.type == "twitch")[0].name}`,
+                                    osu: null
+                                });
+                                discordUsers.push(user.id);
+                            }
+                        });
+                    });
+                });
+            });
+
+            res.send("<script>window.close()</script>");
+        });
     });
 })();
+
+function liveStatus(channel) {
+    return new Promise(async (resolve) => {
+        if(!twitch_accessToken) {
+            await fetch(`https://id.twitch.tv/oauth2/token?client_id=${process.env.TWITCH_CLIENT_ID}&client_secret=${process.env.TWITCH_CLIENT_SECRET}&grant_type=client_credentials`, { 
+                method: "POST"
+            }).then(async result => {
+                result = await result.json();
+                twitch_accessToken = result.access_token;
+            });
+        }
+
+        await fetch(`https://api.twitch.tv/helix/streams?user_login=${channel}`, {
+            method: "GET",
+            headers: {
+                "Accept": "application/json",
+                "Content-Type": "application/json",
+                "Authorization": `Bearer ${twitch_accessToken}`,
+                "Client-Id": process.env.TWITCH_CLIENT_ID
+            }
+        }).then(async result => {
+            result = await result.json();
+            resolve(result.data.length >= 1 ? true : false);
+        });
+    });
+}
+
+function lookupBeatmap(beatmapName) {
+    return new Promise(async (resolve) => {
+        if(!osu_accessToken) {
+            await fetch(`https://osu.ppy.sh/oauth/token`, {
+                method: "POST",
+                body: JSON.stringify({
+                    client_id: process.env.OSU_CLIENT_ID,
+                    client_secret: process.env.OSU_CLIENT_SECRET,
+                    grant_type: "client_credentials",
+                    scope: "public"
+                }),
+                headers: {
+                    "Accept": "application/json",
+                    "Content-Type": "application/json"
+                }
+            }).then(async result => {
+                result = await result.json();
+                osu_accessToken = result.access_token;
+            });
+        }
+
+        await fetch(`https://osu.ppy.sh/api/v2/beatmapsets/search?m=0&q=${beatmapName}&s=any`, {
+            method: "GET",
+            headers: {
+                "Authorization": `Bearer ${osu_accessToken}`,
+                "Content-Type": "application/json",
+                "Accept": "application/json"
+            }
+        }).then(async result => {
+            result = await result.json();
+            for(let i in result.beatmapsets) {
+                let map = result.beatmapsets[i];
+                if(map.artist.match(/\w+.\w+/) && map.artist.match(/\w+.\w+/)[0] == beatmapName.match(/\w+.\w+/)[0]) {
+                    let foundMap = map.beatmaps.find(o => o.version == beatmapName.match(/(?!.*\[)(?<=\[).+?(?=\])/)[0]);
+                    if(foundMap) {
+                        resolve(foundMap);
+                    }
+                }
+            }
+        })
+    });
+}
