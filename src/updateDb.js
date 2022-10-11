d5d0e4c3adaa2b97c99d418a45a3ad65e57aef5f
nzxl <yyy.noel.yyy@gmail.com>
2022-10-11 07:07:09 +0200

    Huge

---
+++ src/updateDb.js
@@ -0,0 +1,37 @@
+require("dotenv").config();
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const mongoClient = new MongoClient(process.env.MONGODB, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1, monitorCommands: true });
+const { BeatmapCalculator } = require("@kionell/osu-pp-calculator");
+const pp = new BeatmapCalculator();
+
+(() => {
+    mongoClient.connect(async err => {
+        if(err) return console.log("MongoDB failed!");
+        db = mongoClient.db("osu");
+        mapData = db.collection("map_data");
+
+        let mapIds = await mapData.distinct("setId");
+        mapIds.forEach((m, i) => {
+            setTimeout(() => {
+                mapData.findOne({ setId: m }).then(async r => {
+                    try {
+                        if(r.name) return;
+
+                        let map = await pp.calculate({ fileURL: `https://osu.ppy.sh/osu/${r.setId}` });
+                        if(map.beatmapInfo.version == r.mapData.version) {
+                            mapName = `${map.beatmapInfo.artist} - ${map.beatmapInfo.title} [${map.beatmapInfo.version}]`;
+                            await mapData.updateOne({ setId: m }, { $set: { name: mapName }});
+
+                            console.log(`${map.beatmapInfo.artist} - ${map.beatmapInfo.title} [${map.beatmapInfo.version}] ${(i+"/"+mapIds.length)}`);
+                        }
+
+                    } catch (err) {
+                        console.log(err);
+                    }
+                });
+            }, (i+1)*1000);
+        });
+
+        console.log("Recalculating..");
+    });
+})();
