e5218054ea71d3ed423c0a2dade266f977e1391a
nzxl101 <yyy.noel.yyy@gmail.com>
2022-11-08 02:25:07 +0100

    Update updateDb.js

--- src/utils/updateDb.js
+++ src/utils/updateDb.js
@@ -4,6 +4,15 @@ const mongoClient = new MongoClient(process.env.MONGODB, { useNewUrlParser: true
 const { BeatmapCalculator } = require("@kionell/osu-pp-calculator");
 const pp = new BeatmapCalculator();
 
+const Banchojs = require("bancho.js");
+const banchoClient = new Banchojs.BanchoClient(
+    {
+        username: "kiyomii",
+        password: "123",
+        apiKey: process.env.OSU_API_KEY
+    }
+);
+
 (() => {
     mongoClient.connect(async err => {
         if(err) return console.log("MongoDB failed!");
@@ -14,22 +23,36 @@ const pp = new BeatmapCalculator();
         mapIds.forEach((m, i) => {
             setTimeout(() => {
                 mapData.findOne({ setId: m }).then(async r => {
-                    try {
-                        if(r.name) return;
+                    // try {
+                    //     if(r.name) return;
 
-                        let map = await pp.calculate({ fileURL: `https://osu.ppy.sh/osu/${r.setId}` });
-                        if(map.beatmapInfo.version == r.mapData.version) {
-                            let mapName = `${map.beatmapInfo.artist} - ${map.beatmapInfo.title} [${map.beatmapInfo.version}]`;
-                            await mapData.updateOne({ setId: m }, { $set: { name: mapName }});
+                    //     let map = await pp.calculate({ fileURL: `https://osu.ppy.sh/osu/${r.setId}` });
+                    //     if(map.beatmapInfo.version == r.mapData.version) {
+                    //         let mapName = `${map.beatmapInfo.artist} - ${map.beatmapInfo.title} [${map.beatmapInfo.version}]`;
+                    //         await mapData.updateOne({ setId: m }, { $set: { name: mapName }});
 
-                            console.log(`${map.beatmapInfo.artist} - ${map.beatmapInfo.title} [${map.beatmapInfo.version}] ${(i+"/"+mapIds.length)}`);
-                        }
+                    //         console.log(`${map.beatmapInfo.artist} - ${map.beatmapInfo.title} [${map.beatmapInfo.version}] ${(i+"/"+mapIds.length)}`);
+                    //     }
 
-                    } catch (err) {
-                        console.log(err);
+                    // } catch (err) {
+                    //     console.log(err);
+                    // }
+
+                    // if(r.setId !== r.mapData.beatmapset_id) {
+                    //     await mapData.updateOne({ setId: m }, { $set: { setId: r.mapData.beatmapset_id }});
+                    //     console.log(i);
+                    // }
+
+
+                    if(!r.mapData["genre"]) {
+                        let map = await banchoClient.osuApi.beatmaps.getBySetId(r.setId);
+                        if(!map || map.length <= 0) return;
+
+                        await mapData.updateOne({ setId: m }, { $set: { "mapData.genre": map[0].genre }});
+                        console.log(i);
                     }
                 });
-            }, (i+1)*1000);
+            }, (i+1)*1500);
         });
 
         console.log("Recalculating..");
